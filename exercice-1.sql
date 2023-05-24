7e73e394dcb61b547e5e61ba30fe51f0fad6107b
Maximebtz <120190748+Maximebtz@users.noreply.github.com>
2023-05-24 17:08:24 +0200

    Update

---
+++ exercice-1.sql
@@ -0,0 +1,92 @@
+-- ETUDIANT (N°ETUDIANT, NOM, PRENOM) 
+-- MATIERE (CODEMAT, LIBELLEMAT, COEFFMAT) 
+-- EVALUER (N°ETUDIANT*, CODEMAT*, DATE, NOTE)
+
+-- Affichez les résultats suivants avec une solution SQL :
+
+    -- a) Quel est le nombre total d'étudiants ?
+        
+        SELECT etudiant_id
+        FROM etudiant 
+
+
+    -- b) Quelles sont, parmi l'ensemble des notes, la note la plus haute et la note la plus basse ?
+        
+        SELECT MIN(note), MAX(note)
+        FROM evaluer
+
+
+    -- c) Quelles sont les moyennes de chaque étudiant dans chacune des matières ? (utilisez CREATE VIEW)
+      
+      -- Utilisation d'une vue   
+        CREATE VIEW MoyennesEtudiants AS
+      -- Selection des colonnes "nom" et "prenom" de la table "etudiant", la colonne "libellemat" de la table matiere utilisatien de SUM() et des notes + coef pour faire le caclcul de moyennes ainsi que "AS MOYENNE" pour donner un nom altérnatif à la colonne calculée  
+        SELECT e.nom, e.prenom, m.libellemat, SUM(ev.note * m.coeffmat) / SUM(m.coeffmat) AS moyenne
+      -- Tables à partir des quelles nous selectionnons les données  
+        FROM etudiant e
+      -- ON == jointure lors de combinaison de plusieur stables, = == cmparer les valeurs des colonnes correspondantes lors de la jointure  
+        JOIN evaluer ev ON e.etudiant_id = ev.etudiant_id
+        JOIN matiere m ON ev.codemat = m.codemat
+      -- Groupement des colonnes pour obtenir une ligne  
+        GROUP BY e.etudiant_id, e.nom, e.prenom, m.libellemat;
+
+      -- * Indique que nous souhaitons retounrer toute les colonnes de 'Moyenne'  
+        SELECT *
+        FROM Moyennes;
+    
+
+    -- d) Quelles sont les moyennes par matière ? (cf. question c)
+    
+        CREATE VIEW MoyennesMatimatieres AS
+        SELECT m.libellemat, SUM(EV.NOTE * M.COEFFMAT) / SUM(M.COEFFMAT) AS moyenne
+        FROM etudiant e
+        JOIN evaluer ev ON e.etudiant_id = ev.etudiant_id
+        JOIN matiere m ON ev.codemat = m.codemat
+        GROUP BY m.libellemat;
+
+        SELECT *
+        FROM MoyennesMatieres;
+
+    
+    -- e) Quelle est la moyenne générale de chaque étudiant ? (utilisez CREATE VIEW + cf. question 3)   
+
+        CREATE VIEW MoyennesGeneralEtudiants AS
+        SELECT e.nom, e.prenom, e.etudiant_id, SUM(EV.NOTE * M.COEFFMAT) / SUM(M.COEFFMAT) AS moyenne_generale
+        FROM etudiant e
+        JOIN evaluer ev ON e.etudiant_id = ev.etudiant_id
+        JOIN matiere m ON ev.codemat = m.codemat
+        GROUP BY e.etudiant_id, e.nom, e.prenom;
+
+        SELECT *
+        FROM MoyennesGeneralEtudiants;
+
+   
+    -- f) Quelle est la moyenne générale de la promotion ? (cf. question e)
+    
+        CREATE VIEW MoyennesGeneralEtudiants AS
+        SELECT e.nom, e.prenom, e.etudiant_id, SUM(EV.NOTE * M.COEFFMAT) / SUM(M.COEFFMAT) AS moyenne_generale
+        FROM etudiant e
+        JOIN evaluer ev ON e.etudiant_id = ev.etudiant_id
+        JOIN matiere m ON ev.codemat = m.codemat
+        GROUP BY e.etudiant_id, e.nom, e.prenom;
+
+        SELECT AVG(moyenne_generale) AS moyenne_generale_promotion
+        FROM MoyennesGeneralEtudiants;
+    
+
+    -- g) Quels sont les étudiants qui ont une moyenne générale supérieure ou égale à la moyenne générale de la promotion ? (cf. question e)
+
+        CREATE VIEW MoyennesGeneralEtudiants AS
+        SELECT e.nom, e.prenom, e.etudiant_id, SUM(EV.NOTE * M.COEFFMAT) / SUM(M.COEFFMAT) AS moyenne_generale
+        FROM etudiant e
+        JOIN evaluer ev ON e.etudiant_id = ev.etudiant_id
+        JOIN matiere m ON ev.codemat = m.codemat
+        GROUP BY e.etudiant_id, e.nom, e.prenom;
+
+        SELECT *
+        FROM MoyenneGeneraleEtudiants
+      -- Chercher où les moyennes sont sup u egales et reprendre le SELECT avg(m_g) ainsi que le FROM M.G.E. utilisés haut dessus 
+        WHERE moyenne_generale >= (
+        SELECT AVG(moyenne_generale)
+        FROM MoyenneGeneraleEtudiants
+        );
---
+++ exercice-2.sql
@@ -0,0 +1,34 @@
+-- ARTICLES (NOART, LIBELLE, STOCK, PRIXINVENT)
+-- FOURNISSEURS (NOFOUR, NOMFOUR, ADRFOUR, VILLEFOUR)
+-- ACHETER (NOFOUR#, NOART#, PRIXACHAT, DELAI)
+
+-- Affichez les résultats suivants avec une solution SQL :
+
+    -- a) Numéros et libellés des articles dont le stock est inférieur à 10 ?
+    
+    
+    -- b) Liste des articles dont le prix d'inventaire est compris entre 100 et 300 ?
+    
+    
+    -- c) Liste des fournisseurs dont on ne connaît pas l'adresse ?
+    
+    
+    -- d) Liste des fournisseurs dont le nom commence par "STE" ?
+    
+    
+    -- e) Noms et adresses des fournisseurs qui proposent des articles pour lesquels le délai d'approvisionnement est supérieur à 20 jours ?
+    
+    
+    -- f) Nombre d'articles référencés ?
+    
+    
+    -- g) Valeur du stock ?
+    
+    
+    -- h) Numéros et libellés des articles triés dans l'ordre décroissant des stocks ?
+    
+    
+    -- i) Liste pour chaque article (numéro et libellé) du prix d'achat maximum, minimum et moyen ?
+    
+    
+    -- j) Délai moyen pour chaque fournisseur proposant au moins 2 articles ?
